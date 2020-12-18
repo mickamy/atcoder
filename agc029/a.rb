0ba5ea071b2314f3a0dd92d0926b85bb553a6223
mickamy <t.mikami1992@gmail.com>
2020-12-18 22:26:20 +0900

    Fix TLE

--- agc029/a.rb
+++ agc029/a.rb
@@ -8,15 +8,30 @@ WHITE = 'W'
 
 text = S
 
-reg = Regexp.new "#{BLACK}#{WHITE}"
-flip = "#{WHITE}#{BLACK}"
+# 全件走査では TLE で NG
+# 
+# reg = Regexp.new "#{BLACK}#{WHITE}"
+# flip = "#{WHITE}#{BLACK}"
 
-iteration = 0
-matched = text.slice reg
-while matched
-  text = text.sub(reg, flip)
-  matched = text.slice reg
-  iteration += 1
+# iteration = 0
+# matched = text.slice reg
+# while matched
+#   text = text.sub(reg, flip)
+#   matched = text.slice reg
+#   iteration += 1
+# end
+# p iteration
+
+# W を左へ動かしていく
+
+black_on_left = 0
+ans = 0
+text.each_char do |char|
+  if char == BLACK
+    black_on_left += 1
+  else
+    ans += black_on_left
+  end
 end
 
-p iteration
+puts ans
