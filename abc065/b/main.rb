6cd6f244d9ac386a33cead446444caddf9c9a8e3
mickamy <t.mikami1992@gmail.com>
2020-12-21 19:15:17 +0900

    Implement abc065 b for a try

--- abc065/b/main.rb
+++ abc065/b/main.rb
@@ -1,6 +1,25 @@
 #!/usr/bin/env ruby
-# frozen_string_literal: true
 
-input = gets.chomp.split("")
+N = gets.chomp.to_i
+ARR = N.times.map do
+  gets.chomp.to_i
+end
 
-p input
+IMPOSSIBLE = -1
+
+START_INDEX = 0
+GOAL_INDEX = 1
+
+ans = 0
+index = START_INDEX
+
+N.times do
+  index = ARR[index] - 1
+  ans += 1
+  if index == GOAL_INDEX
+    puts ans
+    exit
+  end
+end
+
+puts IMPOSSIBLE
