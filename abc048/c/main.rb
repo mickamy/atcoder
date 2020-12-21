43638b03490a0c329f46990ba23cd584e76f9637
mickamy <t.mikami1992@gmail.com>
2020-12-21 20:51:37 +0900

    Complete c

--- abc048/c/main.rb
+++ abc048/c/main.rb
@@ -1,5 +1,26 @@
 #!/usr/bin/env ruby
 
-input = gets.chomp.split.map(&:to_i)
+N, X = gets.chomp.split.map(&:to_i)
+A = gets.chomp.split.map(&:to_i)
 
-p input
+=begin
+O: 任意の A より -1
+
+goal: どの隣り合う A element において、総和が x 以下
+=end
+
+
+ans = 0
+prev = 0
+A.each do |a|
+  sum = prev + a
+  cost = 0
+  if sum > X
+    cost = sum - X
+  end
+
+  ans += cost
+  prev = a - cost
+end
+
+puts ans
