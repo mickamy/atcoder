210aec0f2ea528f184d20c765b17a773790b140b
mickamy <t.mikami1992@gmail.com>
2020-12-22 20:13:06 +0900

    tle

--- abc127/d/main.rb
+++ abc127/d/main.rb
@@ -1,5 +1,25 @@
 #!/usr/bin/env ruby
 
-input = gets.chomp.split.map(&:to_i)
+N, M = gets.chomp.split.map(&:to_i)
+A = gets.chomp.split.map(&:to_i)
+OPERATIONS = []
+M.times.map do |_|
+  b, c = gets.chomp.split.map(&:to_i)
+  OPERATIONS << { b: b, c: c }
+end
 
-p input
+=begin
+O: カードを Bj 枚まで選ぶ(0 枚でもよい)。選んだカードに書かれている整数をそれぞれ Cj に書き換える。
+M 回の操作終了後に N 枚のカードに書かれた整数の合計の最大値を求めてください。
+=end
+
+cards = A
+OPERATIONS.each do |o|
+  b = o[:b]
+  c = o[:c]
+  cards += Array.new(b, c)
+end
+
+ans = cards.sort.reverse.first(N).sum
+
+puts ans
