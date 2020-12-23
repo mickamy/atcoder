ca1bfe5cab3065bebd80c858a633cd1f87e1550d
mickamy <t.mikami1992@gmail.com>
2020-12-23 21:14:25 +0900

    Complete abc127 d

--- abc127/d/main.rb
+++ abc127/d/main.rb
@@ -5,7 +5,8 @@ A = gets.chomp.split.map(&:to_i)
 OPERATIONS = []
 M.times.map do |_|
   b, c = gets.chomp.split.map(&:to_i)
-  OPERATIONS << { b: b, c: c }
+  # sort する際にやりやすいので逆転させる
+  OPERATIONS << [c, b]
 end
 
 =begin
@@ -13,13 +14,25 @@ O: カードを Bj 枚まで選ぶ(0 枚でもよい)。選んだカードに書
 M 回の操作終了後に N 枚のカードに書かれた整数の合計の最大値を求めてください。
 =end
 
-cards = A
-OPERATIONS.each do |o|
-  b = o[:b]
-  c = o[:c]
-  cards += Array.new(b, c)
+def solve
+  A.sort!
+  OPERATIONS.sort!
+
+  ans = 0
+  count = 0
+  while count < N do
+    if !OPERATIONS.empty? && A.last < OPERATIONS.last.first
+      c, b = OPERATIONS.pop
+      times = (count + b <= N) ? b : (N - count)
+      ans += c * times
+      count += times
+    else
+      ans += A.pop
+      count += 1
+    end
+  end
+
+  puts ans
 end
 
-ans = cards.sort.reverse.first(N).sum
-
-puts ans
+solve
