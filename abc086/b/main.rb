416ce923ec728d649f6a34b05cda19bdfcafaa85
mickamy <t.mikami1992@gmail.com>
2020-12-23 21:25:09 +0900

    Complete abc086 b

--- abc086/b/main.rb
+++ abc086/b/main.rb
@@ -1,5 +1,21 @@
 #!/usr/bin/env ruby
 
-input = gets.chomp.split.map(&:to_i)
+require 'complex'
 
-p input
+a, b = gets.chomp.split.map(&:to_i)
+
+num = (a.to_s + b.to_s).to_i
+
+def full_search(num)
+  max = Math.sqrt(num).floor
+  ans = 'No'
+  1.upto(max) do |candidate|
+    if candidate ** 2 == num
+      ans = 'Yes'
+      break
+    end
+  end
+  ans
+end
+
+puts full_search(num)
