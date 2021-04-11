3a1c7790be9a65f254c19095dfbdc3ee29d9336a
mickamy <t.mikami1992@gmail.com>
2021-04-11 20:56:05 +0900

    wip

--- abc197/c/main.rb
+++ abc197/c/main.rb
@@ -3,7 +3,7 @@
 N = gets.chomp.to_i
 A = gets.chomp.split(' ').map(&:to_i)
 
-min = nil
+min = Float::INFINITY
 
 def total_or(arr)
   arr.inject(0) do |acc, ele|
@@ -11,16 +11,26 @@ def total_or(arr)
   end
 end
 
-1.upto(N - 1) do |i|
-  a = A.slice(0, i)
-  b = A.slice(i, N)
-  
-  tmp = total_or(a) ^ total_or(b)
-  if min
-    min = [tmp, min].min
-  else
-    min = tmp
+def total_xor(arr)
+  arr.inject(0) do |acc, ele|
+    acc ^ ele
   end
+end
+
+[0, 1].repeated_permutation(N) do |bits|
+  cur = 0
+  arr = []
+  bits.each_with_index do |bit, index|
+    next if bit == 0
+      
+    slice = A.slice(cur, index)
+    arr << total_or(slice)
+    cur = index
+  end
+
+  set_xor = total_xor(arr)
+  
+  min = [set_xor, min].min
 end
 
 puts min
