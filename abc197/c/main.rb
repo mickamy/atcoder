f4ca456662cf7797a7ffe06701ea1b0f2908b04c
mickamy <t.mikami1992@gmail.com>
2021-04-11 20:38:19 +0900

    wip

--- abc197/c/main.rb
+++ abc197/c/main.rb
@@ -1,3 +1,26 @@
 #!/usr/bin/env ruby
 
-input = gets.chomp.split('')
+N = gets.chomp.to_i
+A = gets.chomp.split(' ').map(&:to_i)
+
+min = nil
+
+def total_or(arr)
+  arr.inject(0) do |acc, ele|
+    acc | ele
+  end
+end
+
+1.upto(N - 1) do |i|
+  a = A.slice(0, i)
+  b = A.slice(i, N)
+  
+  tmp = total_or(a) ^ total_or(b)
+  if min
+    min = [tmp, min].min
+  else
+    min = tmp
+  end
+end
+
+puts min
--- abc197/d/main.rb
+++ abc197/d/main.rb
@@ -1,3 +1,26 @@
 #!/usr/bin/env ruby
 
-input = gets.chomp.split('')
+require 'matrix'
+
+N = gets.chomp.to_i
+x0, y0 = gets.chomp.split(' ').map(&:to_f)
+xn2, yn2 = gets.chomp.split(' ').map(&:to_f)
+
+center_x = (x0 + xn2) / 2.0
+center_y = (y0 + yn2) / 2.0
+
+rotate = 360/N.to_f
+radian = (rotate * Math::PI / 180.0)
+
+a = Math.cos(radian) 
+b = -Math.sin(radian)
+c = Math.sin(radian) 
+d = Math.cos(radian)
+
+e = x0 - center_x
+f = y0 - center_y
+
+x = a * e + b * f + center_x
+y = c * e + d * f + center_y
+
+puts [x, y].map(&:to_s).join(' ')
